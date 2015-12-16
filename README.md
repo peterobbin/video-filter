8b05115b091e94eb5f8fd2c69caf7100142c4690
peterobbin <beijingwang@gmail.com>
2015-12-16 00:12:08 -0500

    performance update

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
 # Another Video Filter
 This openFramework based app is intend to do realtime video effects. The final goal is not only add the effects but also export the video to an actual file.
 
+## Usage
+Call ShaderEffects class to add effects. 
+In order to add effects, an ofFbo object is required to process the video.
+
+## Feature
+- Realtime Effects
+- Different filters 
+- Easy drag & drop integration 
+
 ## TODO
-- Make adding removing effects all happen in output, so the picture will only draw once
+- Video Mixer
 - Record and export
+- Face tracking
 - Depth Camera Integration
+- Adding previous effects from of-light-fx
 
 ## Possible Effects:
 - Color correction 
 - Bloom
 - Lens Effects (calculating brightest pixels)
 - Depth Blur for Kinect Image
-- Distortions
 - Face Block?
 
 
--- src/ShaderFX.cpp
+++ src/ShaderFX.cpp
@@ -30,53 +30,54 @@ void ShaderFX::update(){
 
 void ShaderFX::blur(float bluramt, ofFbo &output){
     
-        ofFbo fboBlurOnePass;
-        ofFbo fboBlurTwoPass;
-        
-        fboBlurOnePass.allocate(output.getWidth(), output.getHeight());
-        fboBlurTwoPass.allocate(output.getWidth(), output.getHeight());
+    ofFbo fboBlurOnePass;
+    ofFbo fboBlurTwoPass;
+    
+    fboBlurOnePass.allocate(output.getWidth(), output.getHeight());
+    fboBlurTwoPass.allocate(output.getWidth(), output.getHeight());
+    
+    float blur = bluramt;
 
+    // horizonal pass
+    //----------------------------------------------------------
+    fboBlurOnePass.begin();
+    shaderBlurX.begin();
+    shaderBlurX.setUniform1f("blurAmnt", blur);
+    output.draw(0, 0);
+    shaderBlurX.end();
+    fboBlurOnePass.end();
         
-        float blur = bluramt;
-
-        // horizonal pass
-        //----------------------------------------------------------
-        fboBlurOnePass.begin();
-        shaderBlurX.begin();
-        shaderBlurX.setUniform1f("blurAmnt", blur);
-        output.draw(0, 0);
-        shaderBlurX.end();
-        fboBlurOnePass.end();
+    // vertical pass
+    //----------------------------------------------------------
+    fboBlurTwoPass.begin();
+    shaderBlurY.begin();
+    shaderBlurY.setUniform1f("blurAmnt", blur);
+    fboBlurOnePass.draw(0, 0);
+    shaderBlurY.end();
+    fboBlurTwoPass.end();
         
-        // vertical pass
-        //----------------------------------------------------------
-        fboBlurTwoPass.begin();
-        shaderBlurY.begin();
-        shaderBlurY.setUniform1f("blurAmnt", blur);
-        fboBlurOnePass.draw(0, 0);
-        shaderBlurY.end();
-        fboBlurTwoPass.end();
+    //----------------------------------------------------------
         
-        //----------------------------------------------------------
-        
-        output = fboBlurTwoPass;
+    output = fboBlurTwoPass;
 }
 
 
 void ShaderFX::bw(float bwShift, ofFbo &output){
-        output.begin();
-        shaderBW.begin();
-        shaderBW.setUniform1f("u_bwshift", bwShift);
-        shaderBW.setUniform2f("u_resolution", ofGetWidth(), ofGetHeight());
-        shaderBW.setUniform2f("u_tex0Resolution", output.getWidth(), output.getHeight());
-        output.draw(0, 0);
-        shaderBW.end();
-        output.end();
+    ofFbo bwFX;
+    bwFX.allocate(output.getWidth(), output.getHeight());
+    bwFX.begin();
+    shaderBW.begin();
+    shaderBW.setUniform1f("u_bwshift", bwShift);
+    shaderBW.setUniform2f("u_resolution", ofGetWidth(), ofGetHeight());
+    shaderBW.setUniform2f("u_tex0Resolution", output.getWidth(), output.getHeight());
+    output.draw(0, 0);
+    shaderBW.end();
+    bwFX.end();
+    output = bwFX;
 
 }
 
 void ShaderFX::distortion(ofFbo &output){
-    
     ofFbo distortFX;
     distortFX.allocate(output.getWidth(), output.getHeight());
     distortFX.begin();
@@ -88,12 +89,13 @@ void ShaderFX::distortion(ofFbo &output){
     shaderDistortion.setUniform2f("u_resolution", ofGetWidth(), ofGetHeight());
     shaderDistortion.setUniform2f("u_tex0Resolution", output.getWidth(), output.getHeight());
     output.draw(0, 0);
-
+    
     shaderDistortion.end();
     distortFX.end();
     //cout<<ofGetElapsedTimef()<<endl;
     
     output = distortFX;
+
 }
 
 void ShaderFX::rawOutput(ofVideoPlayer &vid, ofFbo &output){
